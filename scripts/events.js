95574db17568ad1cb6ec33a3995193beab2132df
Chad <chdabney42@gmail.com>
2020-07-02 13:01:04 -0400

    finished

---
+++ scripts/events.js
@@ -0,0 +1,20 @@
+addButton.addEventListener('click', function () {
+    const x = Number(xInput.value)
+    const y = Number(yInput.value)
+
+    outputElement.innerHTML = calculator(x, y, add)
+})
+
+multiplyButton.addEventListener('click', function () {
+    const x = Number(xInput.value)
+    const y = Number(yInput.value)
+
+    outputElement.innerHTML = calculator(x, y, multiply)
+})
+
+divideButton.addEventListener('click', function () {
+    const x = Number(xInput.value)
+    const y = Number(yInput.value)
+
+    outputElement.innerHTML = calculator(x, y, divide)
+})
